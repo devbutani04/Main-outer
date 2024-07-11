b04fd856f9d5240220841f7aa2d4b2be498c1230
devbutani04 <168613965+devbutani04@users.noreply.github.com>
2024-07-11 20:31:52 +0530

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://quillixinner.vercel.app/';
+        iframe.src = 'https://web-ecru-kappa.vercel.app/';
         /**
          * Use dev server is query params are present
          *
