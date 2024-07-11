30ffaff9d1dc728a724a4aa630817b855192301f
devbutani04 <168613965+devbutani04@users.noreply.github.com>
2024-07-11 20:19:36 +0530

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -194,7 +194,7 @@ export default class MonitorScreen extends EventEmitter {
          */
         const urlParams = new URLSearchParams(window.location.search);
         if (urlParams.has('dev')) {
-            iframe.src = 'http://localhost:3000/';
+            iframe.src = 'https://web-ecru-kappa.vercel.app/';
         }
         iframe.style.width = this.screenSize.width + 'px';
         iframe.style.height = this.screenSize.height + 'px';
