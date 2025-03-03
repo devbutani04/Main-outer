bc9b8e834761514577e53b294c0d1088116d1962
devbutani04 <168613965+devbutani04@users.noreply.github.com>
2025-03-03 14:25:01 +0530

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://web-ecru-kappa.vercel.app/';
+        iframe.src = 'https://quillixinner.vercel.app/';
         /**
          * Use dev server is query params are present
          *
@@ -194,7 +194,7 @@ export default class MonitorScreen extends EventEmitter {
          */
         const urlParams = new URLSearchParams(window.location.search);
         if (urlParams.has('dev')) {
-            iframe.src = 'https://web-ecru-kappa.vercel.app/';
+            iframe.src = 'https://quillixinner.vercel.app/';
         }
         iframe.style.width = this.screenSize.width + 'px';
         iframe.style.height = this.screenSize.height + 'px';
